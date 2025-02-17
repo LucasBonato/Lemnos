0b0e8fc014d55fe310cba0a26ca754fe374c8952
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-02-16 22:32:09 -0300

    [REFACTOR] Resume text in the about page

--- src/pages/aboutPage/About.jsx
+++ src/pages/aboutPage/About.jsx
@@ -94,18 +94,6 @@ export default function About() {
                         Desde sua fundação em 2023, a Lemnos lidera o mercado
                         tecnológico com a sua inovação, oferecendo uma ampla
                         gama de produtos de ponta.
-                        <br />
-                        <br />
-                        Comprometida em tornar a tecnologia mais acessível, a
-                        empresa garante produtos de alta qualidade e uma
-                        experiência de compra excepcional, com uma equipe
-                        especializada que busca constantemente expandir seus
-                        serviços e produtos.
-                        <br />
-                        <br />
-                        Bem-vindo à Lemnos, onde a paixão pela tecnologia e a
-                        busca pela excelência definem o futuro da inovação
-                        tecnológica.
                     </p>
                     <img
                         className="logoDark"
@@ -125,30 +113,25 @@ export default function About() {
                     <div className="item">
                         <h3>Missão</h3>
                         <p>
-                            Na Lemnos, buscamos democratizar o acesso à
+                            Buscamos democratizar o acesso à
                             tecnologia através de produtos inovadores a preços
-                            acessíveis. Nosso compromisso é oferecer produtos de
-                            alta qualidade que conectem e capacitem nossos
-                            clientes em todas as áreas de suas vidas.
+                            acessíveis.
                         </p>
                     </div>
                     <div className="item">
                         <h3>Plataforma</h3>
                         <p>
-                            A plataforma Lemnos oferece uma vitrine digital
+                            Oferecemos uma vitrine digital
                             intuitiva com uma ampla gama de produtos
-                            tecnológicos de ponta, proporcionando uma
-                            experiência de compra simplificada e personalizada.
+                            tecnológicos de ponta.
                         </p>
                     </div>
                     <div className="item">
                         <h3>Valores</h3>
                         <p>
-                            Na Lemnos, priorizamos a excelência e a satisfação
+                            Priorizamos a excelência e a satisfação
                             do cliente, oferecendo tecnologia e soluções
-                            adaptadas às suas necessidades. Nosso ambiente
-                            acolhedor e informativo atende tanto entusiastas
-                            quanto iniciantes.
+                            adaptadas às suas necessidades.
                         </p>
                     </div>
                 </div>
@@ -163,20 +146,10 @@ export default function About() {
 
                 <div className="content">
                     <p className="text">
-                        Na Lemnos, nosso mascote, TechFesto, personifica a fusão
+                        TechFesto, personifica a fusão
                         entre a mitologia antiga e a inovação contemporânea,
                         simbolizando nossa busca pela excelência e progresso
                         tecnológico.
-                        <br />
-                        <br />
-                        Como guardião tecnológico, ele embarca em uma jornada
-                        com nossos clientes, utilizando seu conhecimento para
-                        desvendar novas possibilidades.
-                        <br />
-                        <br />
-                        Junte-se a TechFesto e à comunidade Lemnos na busca pela
-                        excelência e inovação, onde a inspiração mitológica
-                        impulsiona a tecnologia do futuro.
                     </p>
                     <img
                         src={TechFesto}
