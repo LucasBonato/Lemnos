eb3cebfbb5c2b2bfc4e16e1e3eccae9764e7fe35
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-02-16 23:10:18 -0300

    [REFACTOR] Move constants to a diff file

--- src/pages/productFilterPage/ProductFilter.jsx
+++ src/pages/productFilterPage/ProductFilter.jsx
@@ -10,30 +10,7 @@ import AuthService from '../../services/AuthService';
 import Card from '../../components/card/Card';
 import './productFilter.scss';
 
-const BRANDS = [
-    'Acer', 'AMD', 'Apple', 'Asus', 'Dell', 'Gigabyte', 'Logitech', 'Intel',
-    'JBL', 'Lenovo', 'LG', 'Microsoft', 'NVIDIA', 'Philips', 'Positivo',
-    'Samsung', 'Sony', 'Xiaomi',
-];
-
-const CATEGORIES = [
-    'Casa Inteligente', 'Computadores', 'Eletrônicos', 'Hardware', 'Kits', 'Monitores',
-    'Notebooks e Portáteis', 'Periféricos', 'Realidade Virtual', 'Redes e wireless', 'Video Games',
-];
-
-const SUBCATEGORIES = {
-    'Casa Inteligente': ['Assistente Virtual', 'Controles Smarts', 'Lâmpadas Inteligentes', 'Sensores'],
-    'Computadores': ['Computadores Gamers', 'Computadores Workstation'],
-    'Eletrônicos': ['Acessórios de Console', 'Carregadores', 'Refrigeração', 'Smart Box'],
-    'Hardware': ['Armazenamento', 'Coolers', 'Fonte', 'Memória RAM', 'Placa de Vídeo', 'Placa Mãe', 'Processadores'],
-    'Kits': ['Gamer', 'Periféricos', 'Upgrade'],
-    'Monitores': ['Monitores Gamers', 'Monitores Workstation'],
-    'Notebooks e Portáteis': ['Notebooks', 'Smartphones', 'Tablets'],
-    'Periféricos': ['Caixa de Som', 'Fone de Ouvido', 'Microfone', 'Mouse', 'Mousepad', 'Teclado'],
-    'Realidade Virtual': ['Óculos de VR', 'Periféricos de VR'],
-    'Redes e wireless': ['Access Point', 'Adaptadores', 'Cabos', 'Cabos de Redes', 'Roteadores', 'Switches'],
-    'Video Games': ['Console de Mesa', 'Portátil'],
-};
+import { BRANDS, CATEGORIES, SUBCATEGORIES} from '../../constants/filters';
 
 export default function ProductFilter() {
     const navigate = useNavigate();
